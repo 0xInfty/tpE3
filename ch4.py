01a0d5b52476ccfd945e9743a6434e49de828e35
0xInfty <anirix.vall@gmail.com>
2020-07-23 21:08:16 -0300

    Agrego archivos de CH4.

---
+++ ch4.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+TP ESTRUCTURA DE LA MATERIA 3
+1er cuatrimestre 2020
+-------------------------------
+EJERCICIO 1: Metano
+
+@author: Leandro Fernandez, Valeria Pais
+"""
+
+
+
